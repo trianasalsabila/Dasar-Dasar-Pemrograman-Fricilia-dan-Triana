2f70dff671f0ba806aa6309ec46340d6f34e0a25
trianasalsabila <73945435+trianasalsabila@users.noreply.github.com>
2020-12-11 21:50:42 +0700

    Update Program Kasir Sederhana.py

--- Program Kasir Sederhana.py
+++ Program Kasir Sederhana.py
@@ -1,2 +1,14 @@
 #program kasir sederhana
 #Program Fricillia dan Triana
+
+total1=0
+total2=0
+totalsemua=0
+jenis1=""
+jenis2=""
+
+print("=== Program Kasir Sederhana Warung Laperpool ===")
+nama = input("Masukkan nama Konsumen: ")
+print ("Nama Konsumen :", nama)
+print("")
+print ("Menu Makanan")
